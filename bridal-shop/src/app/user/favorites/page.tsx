df8865c0afadc263de686699432a59094cc0bdef
Alice <alicehata9@gmail.com>
2023-08-31 19:36:46 -0300

    add heart icon

--- bridal-shop/src/app/user/favorites/page.tsx
+++ bridal-shop/src/app/user/favorites/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import { Product } from "@/types/Products";
 import Image from "next/image";
+import HeartIcon from "../../../../public/favorite.svg";
 import { Footer } from "@/components/Footer";
 import { useRouter } from "next/navigation";
 
@@ -28,9 +29,7 @@ export default function Favorites() {
         const res = await api.get(`/user/favorite_prod/${user.sub}`, {
           headers: { Authorization: 'Bearer ' + token }
         })
-        if(res.status == 200){
-          setFavProducts(res.data);
-        }
+        if(res.status == 200){ setFavProducts(res.data);}
         setLoadingProducts(false);
       }catch(error) {
         router.push('/');
@@ -47,13 +46,15 @@ export default function Favorites() {
       <main>
         <div>
           <h1 className="text-center text-main text-title font-bold">Favoritos</h1>
-          <ul className="flex gap-4 p-8">
+          <ul className="flex gap-4 p-8 flex-wrap items-center justify-center md:justify-normal">
             {favProducts ? favProducts.map(fav =>
               <li key={fav.id}>
-                {/* draw star or heart */}
-                <Image src={fav.product.coverUrl} alt={fav.product.name} width={200} height={200} className="border border-main rounded-2xl" />
-                <h3>{fav.product.name}</h3>
-                <p>R${fav.product.price},00</p>
+                <div className="flex flex-col justify-center items-center relative">
+                  <Image src={HeartIcon} alt="" className="absolute z-10 top-2 right-2" width={25} height={25}/>
+                  <Image src={fav.product.coverUrl} alt={fav.product.name} width={200} height={200} className="border border-main rounded-2xl" />
+                  <h3 className="mt-2">{fav.product.name}</h3>
+                  <p>R${fav.product.price},00</p>
+                </div>
               </li>
             ):
             <p>Você não possui vestidos ou acessórios favoritos</p>
